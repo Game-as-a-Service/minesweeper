f53c835af0e10957c5b347fa7cedff4a2f7c67b0
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2022-12-31 15:49:02 -0500

    feat: 建立前後端通訊 (websocket)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { WsGateway } from './app.gateway';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WsGateway],
 })
 export class AppModule {}
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+  const socket = new WebSocket('ws://localhost:3000');
+  socket.onopen = function() {
+    console.log('Connected');
+    socket.send(
+      JSON.stringify({
+        event: 'events',
+        data: 'test',
+      }),
+    );
+    socket.onmessage = function(data) {
+      console.log(data);
+    };
+  };
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
