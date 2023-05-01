709de106c6d6d5c402cf90f31d2897fac083e941
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-05-01 10:41:29 +0800

    feat: 實作 auth，固定帳密可呼叫 Restful API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { WsGateway } from './app.gateway';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env' : '.local.env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: envFile,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService, WsGateway],
 })
