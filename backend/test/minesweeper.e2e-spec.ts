f91fa3dbae8570c44f376bc649c6accaa167f038
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-05-23 19:39:55 +0800

    test: 使用新寫法完成原本的 e2e test 案例

--- backend/test/minesweeper.e2e-spec.ts
+++ backend/test/minesweeper.e2e-spec.ts
@@ -8,16 +8,28 @@ import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from '../src/app.module';
 import * as WebSocket from 'ws';
 import { WinLoseState } from '../src/minesweeper/gameState';
+import { Cell, CellState } from '../src/minesweeper/cell';
+import { WsGateway } from '../src/app.gateway';
+import { Minesweeper } from '../src/minesweeper/minesweeper';
+import { randomUUID } from 'crypto';
+import { MinesweeperData } from '../src/data-services/data/minesweeper.data';
+import { LevelConfig } from '../src/minesweeper/levelConfig';
 
 // [TypeScript Promise - Scaler Topics](https://www.scaler.com/topics/typescript/typescript-promise/)
 describe('Asynchronous WebSocket Code', () => {
   let app: INestApplication;
   let ws: WebSocket;
 
+  // TODO 之後要把 Repository 移到其它地方
+  let wsGateway: WsGateway;
+
   beforeAll(async () => {
     app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new WsAdapter(app));
     app.enableShutdownHooks();
+
+    wsGateway = app.get(WsGateway);
+
     await app.listen(3000);
   });
 
@@ -50,12 +62,13 @@ describe('Asynchronous WebSocket Code', () => {
     sendData('ping', {});
   };
 
-  const gameInfo = () => {
-    sendData('gameInfo', {});
+  const gameInfo = (gameId: string = undefined) => {
+    sendData('gameInfo', { gameId });
   };
 
-  const open = (x: number, y: number) => {
+  const open = (gameId: string, x: number, y: number) => {
     const data = {
+      gameId,
       x,
       y,
     };
@@ -63,8 +76,9 @@ describe('Asynchronous WebSocket Code', () => {
     sendData('open', data);
   };
 
-  const flag = (x: number, y: number) => {
+  const flag = (gameId: string, x: number, y: number) => {
     const data = {
+      gameId,
       x,
       y,
     };
@@ -99,14 +113,201 @@ describe('Asynchronous WebSocket Code', () => {
     expect(event.event).toBe('pong');
   });
 
-  // 基本遊戲狀態
-  it('when game start then gameState should be NONE', async () => {
+  it('遊戲開始後，gameState 應該是 NONE', async () => {
     await onWsOpen();
-    ping();
-    let event = await onWsMessage();
-    expect(event.event).toBe('pong');
     gameInfo();
-    event = await onWsMessage();
+    const event = await onWsMessage();
     expect(event.data.gameState.winLose).toBe(WinLoseState.NONE);
+  });
+
+  it('只能踩還沒有踩過且沒有插旗的格子 - 這個位置還沒踩過且沒有插旗', async () => {
+    await onWsOpen();
+    gameInfo();
+
+    // given
+    // 這個位置還沒踩過且沒有插旗
+    let event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.UNOPENED);
+
+    // when
+    // 玩家踩地雷
+    open(event.data.gameId, 0, 0);
+
+    // then
+    // 這一格被踩過
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.OPENED);
+  });
+
+  it('只能踩還沒有踩過且沒有插旗的格子 - 這個位置已經被踩過', async () => {
+    await onWsOpen();
+    gameInfo();
+
+    let event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.UNOPENED);
+
+    // given
+    // 這個位置已經被踩過
+    open(event.data.gameId, 0, 0);
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.OPENED);
+
+    // when
+    // 玩家踩地雷
+    open(event.data.gameId, 0, 0);
+
+    // then
+    // 沒有變化
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.OPENED);
+  });
+
+  it('只能踩還沒有踩過且沒有插旗的格子 - 這個位置已經被插旗', async () => {
+    await onWsOpen();
+    gameInfo();
+
+    let event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.UNOPENED);
+
+    // given
+    // 這個位置已經被插旗
+    flag(event.data.gameId, 0, 0);
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.FLAGGED);
+
+    // when
+    // 玩家踩地雷
+    open(event.data.gameId, 0, 0);
+
+    // then
+    // 沒有變化
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.FLAGGED);
+  });
+
+  const generateCells = (levelConfig: LevelConfig): Cell[][] => {
+    const cells = [];
+    for (let y = 0; y < levelConfig.size.y; y++) {
+      cells[y] = [];
+      for (let x = 0; x < levelConfig.size.x; x++) {
+        cells[y][x] = new Cell(x, y);
+      }
+    }
+    return cells;
+  };
+
+  /**
+   * 3 x 3，只有 1 個地雷，要自己設定地雷位置
+   */
+  const initData = (): MinesweeperData => {
+    const levelConfig = {
+      size: {
+        x: 3,
+        y: 3,
+      },
+      mineCount: 1,
+    };
+
+    const data: MinesweeperData = {
+      gameId: randomUUID(),
+      gameState: {
+        isPlay: true,
+        winLose: WinLoseState.NONE,
+        displayMineCount: levelConfig.mineCount,
+      },
+      board: {
+        cells: generateCells(levelConfig),
+        unopenedCells:
+          levelConfig.size.x * levelConfig.size.y - levelConfig.mineCount,
+        flagCount: 0,
+      },
+      levelConfig,
+    };
+
+    return data;
+  };
+
+  it('踩到地雷遊戲結束', async () => {
+    // Given
+    const data = initData();
+
+    // 在 0, 0 放地雷
+    data.board.cells[0][0].mine = true;
+
+    const domain: Minesweeper = wsGateway.minesweeperDataModel.toDomain(data);
+    wsGateway.minesweeperRepository.save(domain);
+
+    await onWsOpen();
+    gameInfo(domain.gameId);
+
+    let event = await onWsMessage();
+    expect(event.data.gameState.winLose).toBe(WinLoseState.NONE);
+
+    // When
+    // 玩家踩地雷
+    open(event.data.gameId, 0, 0);
+
+    // Then
+    // 遊戲結束
+    event = await onWsMessage();
+    expect(event.data.gameState.winLose).toBe(WinLoseState.LOSE);
+  });
+
+  it('沒踩到地雷會知道附近有多少地雷', async () => {
+    // Given
+    const data = initData();
+
+    // 在 0, 0 放地雷
+    data.board.cells[0][1].mine = true;
+    data.board.cells[0][0].number = 1;
+
+    const domain: Minesweeper = wsGateway.minesweeperDataModel.toDomain(data);
+    wsGateway.minesweeperRepository.save(domain);
+
+    await onWsOpen();
+    gameInfo(domain.gameId);
+
+    // When
+    // 玩家踩地雷
+    let event = await onWsMessage();
+    open(event.data.gameId, 0, 0);
+
+    // Then
+    // 該格會知道附近有多少地雷
+    event = await onWsMessage();
+    expect(event.data.cells[0][0].number).toBe(1);
+  });
+
+  it('沒踩到地雷且附近也沒有地雷，自動踩附近的所有位置', async () => {
+    // Given
+    const data = initData();
+
+    // 在 2, 2 放地雷
+    data.board.cells[2][2].mine = true;
+
+    const domain: Minesweeper = wsGateway.minesweeperDataModel.toDomain(data);
+    wsGateway.minesweeperRepository.save(domain);
+
+    await onWsOpen();
+    gameInfo(domain.gameId);
+
+    let event = await onWsMessage();
+    expect(event.data.cells[0][0].state).toBe(CellState.UNOPENED);
+    expect(event.data.cells[0][1].state).toBe(CellState.UNOPENED);
+
+    // When
+    // 玩家踩地雷
+    open(event.data.gameId, 0, 0);
+
+    // Then
+    // 自動踩附近的所有位置
+    event = await onWsMessage();
+    for (let y = 0; y < data.levelConfig.size.y; y++) {
+      for (let x = 0; x < data.levelConfig.size.x; x++) {
+        if (x !== 2 && y !== 2) {
+          expect(event.data.cells[y][x].state).toBe(CellState.OPENED);
+        }
+      }
+    }
   });
 });
