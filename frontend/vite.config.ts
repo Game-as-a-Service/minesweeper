540cf419f7ab9564ad4439448f062cdbc6df8276
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-06-10 15:20:28 +0800

    ci: 設定 docker-compose.local.yml 方便本機開發

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
