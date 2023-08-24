5d603a6454c5f3f5b3661b8b25c222879c24704a
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-08-24 13:49:05 +0800

    feat: 調整 layout，將直接登入與大平台串接分開

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import { useUserStore } from "./stores/user";
-import router from "./router";
-// import HelloWorld from "./components/HelloWorld.vue";
 
 const store = useUserStore();
 
@@ -16,115 +14,10 @@ onMounted(() => {
     store.user.token = token;
   }
 });
-
-function logout() {
-  store.logout();
-  router.push({ name: "login" });
-}
 </script>
 
 <template>
-  <header>
-    <!-- <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    /> -->
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/game">Game</RouterLink>
-        <div v-if="store.user.account === ''">
-          <RouterLink to="/login">Login</RouterLink>
-          <RouterLink to="/signup">Signup</RouterLink>
-        </div>
-        <div v-else><a @click="logout">Logout</a></div>
-      </nav>
-    </div>
-  </header>
-  <div class="router-view">
-    <RouterView />
-  </div>
-
-  <!-- <a>登入</a> -->
-
-  <!-- <Minesweeper /> -->
+  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-  width: 200px;
-}
-
-.router-view {
-  width: 700px;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  height: 200px;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-  display: flex;
-  flex-direction: column;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
