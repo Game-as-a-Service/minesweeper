b6ac1ea6460d968c54071f67df0f7c67009f9449
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-23 18:24:14 +0800

    ci: 針對 Socket.IO 調整相關 url 設定

--- frontend/src/common/config.ts
+++ frontend/src/common/config.ts
@@ -3,7 +3,7 @@ const port = 3000;
 const host = "minesweeper.snowbellstudio.com";
 
 export let wsUrl = `wss://${host}:${port}`;
-export let sokcetioUrl = `https://${host}:${port}`;
+export let sokcetioUrl = `https://${host}`;
 
 // for local dev
 if (urlHost === "localhost" || urlHost === "127.0.0.1") {
