3b49b730d495bf28234b85b3956cb16e1c1e64cd
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2023-09-29 13:58:52 +0800

    refactor: 除非不需要的部份

--- frontend/src/layout/AppLayout.vue
+++ frontend/src/layout/AppLayout.vue
@@ -3,7 +3,6 @@ import { onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import { useUserStore } from "@/stores/user";
 import router from "@/router";
-// import HelloWorld from "./components/HelloWorld.vue";
 
 const store = useUserStore();
 
@@ -25,17 +24,7 @@ function logout() {
 
 <template>
   <header>
-    <!-- <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    /> -->
-
     <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -51,10 +40,6 @@ function logout() {
   <div class="router-view">
     <RouterView />
   </div>
-
-  <!-- <a>登入</a> -->
-
-  <!-- <Minesweeper /> -->
 </template>
 
 <style scoped>
@@ -66,11 +51,6 @@ header {
 
 .router-view {
   width: 700px;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
@@ -106,10 +86,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
